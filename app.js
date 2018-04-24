d3b8cdf9fc509cbbfbc895a29b5080c2c7c2281b
Jack Chapman <jackchapman1997@hotmail.co.uk>
2018-04-24 17:58:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+const cors = require('cors')
+app.use(cors())
+
+var path = require('path');
+app.use(express.static(path.join(__dirname + 'public')));
+
+app.post('/electricity', function(req, res) {
+    console.log(req.body);
+    var electricDay = req.body.electricityDay;
+    var electricLow = req.body.electricityLow;
+    
+    const unitDayRate = 0.11;
+    const unitLowRate = 0.075;
+    
+    var costDay = unitDayRate * electricDay;
+    var costLow = unitLowRate * electricLow;
+    
+    res.json(costDay || costLow);
+});
+
+app.listen(8081, function () {});
